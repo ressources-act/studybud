16e0cd961698f1e87bfb70754b7f237454dc12f1
Gabriel Crépeault-Cauchon <gabrielcrepeault@gmail.com>
2019-09-28 21:26:52 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # studybud
 Shiny-based application to help students create custom and flexible study flashcards/cheatcheets via RMarkdown syntax.
+
+
+# How-to use
+`flashcards` folder contains a question/answer bank, with a template.
---
+++ ui.R
@@ -0,0 +1,17 @@
+library(shiny)
+
+# AStuces pour Rmd : https://holtzy.github.io/Pimp-my-rmd/#hide_code
+# Créer ses propres templates : https://bookdown.org/yihui/rmarkdown/document-templates.html
+
+# Fichier pour configurer l'interface utilisateur
+shinyUI(fluidPage(
+    titlePanel("Study buddy"),
+    withMathJax(),
+        mainPanel(
+            h1("Aide à l'étude"),
+            uiOutput('filelst'),
+            hr(),
+            htmlOutput('flashcard')
+        )
+    )
+)
